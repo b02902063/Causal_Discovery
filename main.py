db2e8958e116d544274c18ea7847bedc28d2893a
Chun Wei Chen <fd20090aq@gmail.com>
2023-12-24 00:13:03 +0800

    Gcloud sample code testing

---
+++ main.py
@@ -0,0 +1,27 @@
+import functions_framework
+
+
+# Global variable, modified within the function by using the global keyword.
+count = 0
+
+
+@functions_framework.http
+def test(request):
+    """
+    HTTP Cloud Function that counts how many times it is executed
+    within a specific instance.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    global count
+    count += 1
+
+    # Note: the total function invocation count across
+    # all instances may not be equal to this value!
+    return f"Instance execution count: {count}"
+    
